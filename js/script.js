c70f34fee30f4984fc1ce3123f655f08e0b8bf40
bmqz44 <bmqz44@gmail.com>
2015-07-01 13:48:11 -0700

    responsive nav bar

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var nav = document.getElementById('navLinks');
+var button = document.getElementById('navButton');
+
+(function(){
+	var width = window.innerWidth;
+		if(width >= 768){
+			nav.classList.add('show');
+		} else {
+			nav.classList.add('hide');
+		}
+
+}("docReady", window));
+
+
+button.addEventListener('click', function(){
+	nav.classList.toggle('hide');
+	nav.classList.toggle('show');
+})
+
+window.addEventListener('resize', function(){
+	var width = window.innerWidth;
+		if(width <= 768){
+			nav.classList.add('hide');
+			nav.classList.remove('show');
+		} else {
+			nav.classList.remove('hide');
+			nav.classList.add('show');
+		}
+})
+
